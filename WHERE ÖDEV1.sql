6b0e009554c05c5c8cdca67b9c4b7600e6f2d6b5
Uygar Işık <126560257+UYGARISIK@users.noreply.github.com>
2023-03-07 12:14:35 +0300

    push.homework

---
+++ WHERE ÖDEV1.sql
@@ -0,0 +1,10 @@
+--SELECT  title, description FROM film;
+--SELECT * FROM film 
+--WHERE length >60 and length <75 ;
+--WHERE rental_rate = 0.99 and replacement_cost = 12.99 or replacement_cost = 28.99;
+--SELECT * FROM customer
+--where first_name = 'Mary';
+--SELECT * FROM film
+--WHERE  NOT(NOT ( rental_rate = 4.99 or rental_rate = 2.99)) ;
+--SELECT * FROM film
+--WHERE not length > 50 ;
